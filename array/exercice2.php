4b31b73c5921daa11ae6daf9251f58e2368d2564
marineco <marinecolonge@gmail.com>
2017-01-09 16:56:23 +0100

    exercice 2 check

---
+++ array/exercice2.php
@@ -0,0 +1,9 @@
+<h1>Exercice 2</h1>
+
+<?php 
+
+	$mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+
+	echo $mois[2];
+
+?>
