439f3c1863bf75eee36e86bf094b05a272b7ffc5
marineco <marinecolonge@gmail.com>
2017-01-09 23:24:12 +0100

    exercice 9 check

---
+++ array/exercice9.php
@@ -0,0 +1,19 @@
+<h1>Exercice 5</h1>
+
+<?php 
+
+	$hdf = array(
+
+		02 => 'Aisne',
+		59 => 'Nord',
+		60 => 'Oise',
+		62 => 'Pas-de-Calais',
+		80 => 'Somme'
+
+		);
+
+	foreach ($hdf as $value) {
+		echo '<p> Valeur =' . ' '. $value . ' ' . '</p>';
+	}
+
+?>
