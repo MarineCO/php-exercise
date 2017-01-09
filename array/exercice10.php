15468eb986889bce5de5cfd0b4c055111720aaee
marineco <marinecolonge@gmail.com>
2017-01-09 23:30:22 +0100

    exercice 10 check

---
+++ array/exercice10.php
@@ -0,0 +1,19 @@
+<h1>Exercice 10</h1>
+
+<?php 
+
+	$hdf = array(
+
+		02 => 'Aisne',
+		59 => 'Nord',
+		60 => 'Oise',
+		62 => 'Pas-de-Calais',
+		80 => 'Somme'
+
+		);
+
+	foreach ($hdf as $key => $value) {
+		echo '<p> Département du ' . $key . ' = ' . $value . '</p>';
+	}
+
+?>
