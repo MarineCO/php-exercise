49911d8ba422ca3572913be33a1f73438ccf31d4
marineco <marinecolonge@gmail.com>
2017-01-09 23:17:49 +0100

    exercice 8 check

---
+++ array/exercice8.php
@@ -0,0 +1,10 @@
+<h1>Exercice 8</h1>
+
+<?php 
+
+	$mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+
+	foreach ($mois as $value) {
+		echo '<p> Valeur = ' . $value . ' </p>';
+	}
+?>
