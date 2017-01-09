16c44a442ace7c197778ca1540a151ac14060cd7
marineco <marinecolonge@gmail.com>
2017-01-09 16:57:27 +0100

    exercice 3 check

---
+++ array/exercice3.php
@@ -0,0 +1,9 @@
+<h1>Exercice 3</h1>
+
+<?php 
+
+	$mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+
+	echo $mois[5];
+
+?>
