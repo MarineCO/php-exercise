ba7a6ccaa1e7728847f69f9243106363eded89a8
marineco <marinecolonge@gmail.com>
2017-01-09 17:50:50 +0100

    exercice 7 check

---
+++ array/exercice7.php
@@ -0,0 +1,18 @@
+<h1>Exercice 7</h1>
+
+<?php 
+
+	$hdf = array(
+
+		02 => 'Aisne',
+		59 => 'Nord',
+		60 => 'Oise',
+		62 => 'Pas-de-Calais',
+		80 => 'Somme'
+
+		);
+
+	$hdf[51] = 'Marne';
+
+	var_dump($hdf);
+?>
