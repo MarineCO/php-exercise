e93eb0bb1f143978d212b09dc4f7dce98caca5ed
marineco <marinecolonge@gmail.com>
2017-01-10 14:17:15 +0100

    exercice 6 check

--- form/exercice5.php
+++ form/exercice5.php
@@ -8,20 +8,31 @@
 	
 	<h1>Exercice 5</h1>
 
-	<select name="civilite" id="civilite">
-		<option value="monsieur">Mr</option>
-		<option value="madame">Mme</option>
-	</select>
+<?php
 
-	<form method="POST" action="exercice5.php">
-		<label for="lastname">Nom</label>
-		<input id="lastname" name="lastname" type="text"/>
-		
-		<label for="firstname">Prénom</label>
-		<input id="firstname" name="firstname" type="text"/>
-		
-		<input type="submit" value="Valider">
-	</form>
+	$form = '<form method="POST" action="exercice5.php">
+				<select name="civilite" id="civilite">
+					<option value="Mr">Mr</option>
+					<option value="Mme">Mme</option>
+				</select>
+				<label for="lastname">Nom</label>
+				<input id="lastname" name="lastname" type="text"/>
+				<label for="firstname">Prénom</label>
+				<input id="firstname" name="firstname" type="text"/>
+				<input type="submit" value="Valider"/>
+			</form>';
+	
+	$lastname = $_POST['lastname'];
+	$firstname = $_POST['firstname'];
+	$civilite = $_POST['civilite'];
+
+	if (isset($lastname) && isset($firstname)) {
+		echo '<p>' .$civilite. ' ' .$lastname. ' ' .$firstname. '</p>';
+	} else {
+		echo $form;
+	}
+	
+?>
 
 </body>
 </html>
