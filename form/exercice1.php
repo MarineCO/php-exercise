9f3d4dc3db83ed466b474440a502110f070ae6f1
marineco <marinecolonge@gmail.com>
2017-01-10 11:03:30 +0100

    ajout dossier form + exercice 1 check

---
+++ form/exercice1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercices PHP : form</title>
+</head>
+<body>
+	
+<h1>Exercice 1</h1>
+
+<form method="GET" action="user.php">
+	
+	<label for="name">Nom</label>
+	<input id="name" type="text"/>
+
+	<label for="firstname">Prénom</label>
+	<input id ="firstname" type="text"/>
+
+	<input type="submit" value="Valider"/>
+
+</form>
+
+</body>
+</html>
+
