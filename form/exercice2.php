5e4cacd0d12d3c5224ad0e6e573019051c294194
marineco <marinecolonge@gmail.com>
2017-01-10 11:06:36 +0100

    exercice 2 check

---
+++ form/exercice2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercices PHP : form</title>
+</head>
+<body>
+	
+<h1>Exercice 2</h1>
+
+<form method="POST" action="user.php">
+	
+	<label for="name">Nom</label>
+	<input id="name" type="text"/>
+
+	<label for="firstname">Prénom</label>
+	<input id ="firstname" type="text"/>
+
+	<input type="submit" value="Valider"/>
+
+</form>
+
+</body>
+</html>
