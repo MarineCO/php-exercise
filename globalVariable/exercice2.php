352ee73776bdb0a4966dc84cb89180624f551f77
marineco <marinecolonge@gmail.com>
2017-01-11 12:05:52 +0100

    exercice 2 check

--- globalVariable/exercice2.php
+++ globalVariable/exercice2.php
@@ -6,6 +6,11 @@
 
 	session_start();
 
+	$_SESSION['lastname'] = Colonge;
+	$_SESSION['firstname'] = Marine;
+	$_SESSION['age'] = 27;
+	var_dump($_SESSION['age']);
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,22 +19,21 @@
 </head>
 <body>
 	
-	<h1>Exercice 1</h1>
+	<h1>Exercice 2</h1>
 	
 	<div class="info">
 		<p> User Agent de l'utilisateur = <?= $userAgent ?></p>
 		<p> Adresse ip de l'utilisateur = <?= $ip ?></p>
 		<p> Nom du serveur = <?= $serverName ?></p>
 	</div>
-	
-	<a href="exercice2.php">Lien page exercice 2</a>
+
+	<a href="exercice1.php">Lien page exercice 1</a>
 
 	<div>
 		<p>Mon prénom est <?= $_SESSION['firstname']?></p>
 		<p>Mon nom est <?= $_SESSION['lastname']?></p>
 		<p>Mon age est <?= $_SESSION['age']?> ans</p>
 	</div>
-
 
 </body>
 </html>
