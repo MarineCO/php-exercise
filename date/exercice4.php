23400ad5981f07cce72335306b1cb348367a1e5c
marineco <marinecolonge@gmail.com>
2017-01-11 16:59:20 +0100

    exercice 4 check

---
+++ date/exercice4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercices PHP : date</title>
+</head>
+<body>
+	
+	<h1>Exercice 4</h1>
+
+	 <p>Timestamp du jour = <?= strtotime("now") ?></p>
+	 <p>Timestamp du mardi 2 août 2016 à 15h00 = <?= strtotime("tuesday 2 august 2016 15:00") ?></p>
+
+
+</body>
+</html>
