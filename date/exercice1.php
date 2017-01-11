d8db5b54bbcc7f490ac2570ea2bde2f6b6fbc781
marineco <marinecolonge@gmail.com>
2017-01-11 15:38:50 +0100

    ajout dossier date + exercice 1 check

---
+++ date/exercice1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercices PHP : date</title>
+</head>
+<body>
+	
+	<h1>Exercice 1</h1>
+
+ <p>Date du jour au format jj/mm/aaaa = <?= $today = date("j/m/Y")?></p>
+
+</body>
+</html>
