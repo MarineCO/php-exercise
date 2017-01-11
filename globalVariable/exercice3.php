c7722dad598f2df18ee6fd235244bc67244965f7
marineco <marinecolonge@gmail.com>
2017-01-11 14:03:27 +0100

    séparation exercice 3 et exercice 4 en 2 fichiers

--- globalVariable/exercice3.php
+++ globalVariable/exercice3.php
@@ -1,14 +1,4 @@
-<?php 
-	session_start();
-
-	$login = $_POST['login'];
-	var_dump($login);
-	$password = $_POST['password'];
-
-	setcookie('login', $login, time() + 24*3600, null, null, false, true);
-	setcookie('password', $password, time() + 24*3600, null, null, false, true);
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -18,19 +8,14 @@
 	
 	<h1>Exercice 3</h1>
 
-		<form method="POST" action="exercice3.php">
-			<label for="login">Identifiant</label>
-			<input id="login" name="login" type="text"/>
-			<label for="password">Mot de passe</label>
-			<input id="password" name="password" type="text"/>
+	<form method="POST" action="exercice4.php">
+		<label for="login">Identifiant</label>
+		<input id="login" name="login" type="text"/>
+		<label for="password">Mot de passe</label>
+		<input id="password" name="password" type="text"/>
 
-			<input type="submit" value="Valider"/>
-		</form>
-
-		<?php var_dump($_COOKIE['login']) ?> 
-
-		<p>Mon identifiant est <?= $_COOKIE['login']; ?></p>
-		<p>Mon mot de passe est <?= $_COOKIE['password']; ?></p>
+		<input type="submit" value="Valider"/>
+	</form>
 
 </body>
 </html>
